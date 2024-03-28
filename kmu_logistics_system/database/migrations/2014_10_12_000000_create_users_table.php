9654757f1c2c2bbc774a976dbc69ec66ec065c0b
Nswana123 <nswanacobet@gmail.com>
2024-03-28 10:14:53 +0200

    db with roles

--- kmu_logistics_system/database/migrations/2014_10_12_000000_create_users_table.php
+++ kmu_logistics_system/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->default('super admin');
+            $table->string('role')->default('super admin');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
