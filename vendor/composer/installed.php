328a5812887f5bbc6881936ce4e774f87f7b672f
Samatepafinita <150775081+Samatepafinita@users.noreply.github.com>
2024-04-03 10:03:33 +0200

    fg

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '710fd6f3d3585a5e468b2729178d49b54d2e17fb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '710fd6f3d3585a5e468b2729178d49b54d2e17fb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
